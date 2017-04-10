05565a58ce4494972f9d9289f799902358cc6624
Krzysztof Burkat <burkat.krzysztof@gmail.com>
2017-04-10 10:19:13 +0200

    update

--- TutorialModule/src/Main.java
+++ TutorialModule/src/Main.java
@@ -1,4 +1,4 @@
-
+//* Init class responsible for game's tutorial. Will be implemented in future. */ 
 public class Main {
 	public static void main(String[] args){
 		System.out.println("Hello world");
