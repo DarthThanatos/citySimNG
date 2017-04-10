96ae6a5b96cf66be31e99c0fedec9b68ae941f73
Krzysztof Burkat <burkat.krzysztof@gmail.com>
2017-04-10 10:12:54 +0200

    update info

--- CreatorModule/src/creatorcontroller/CreatorNode.java
+++ CreatorModule/src/creatorcontroller/CreatorNode.java
@@ -1,5 +1,6 @@
-package creatorcontroller;
+//* Init class responsible for creating new games with given parameters. Will be implemented in future. */ 
 
+package creatorcontroller;
 import controlnode.Node;
 
 public class CreatorNode extends Node{
