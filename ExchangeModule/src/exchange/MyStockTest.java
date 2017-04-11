e0bc450d6e6f3e681a9c0ea6a43fc4f536be871d
Krzysztof Burkat <burkat.krzysztof@gmail.com>
2017-04-11 19:28:46 +0200

    exchange update

--- ExchangeModule/src/exchange/MyStockTest.java
+++ ExchangeModule/src/exchange/MyStockTest.java
@@ -1,73 +1,72 @@
 package exchange;
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
 
+/** For testing purposes */
+
 public class MyStockTest extends JFrame implements Runnable {
 
-  Stock[] market = {
-    new Stock("JTree", 14.57),
-    new Stock("JTable", 17.44),
-    new Stock("JList", 16.44),
-    new Stock("JButton", 7.21),
-    new Stock("JComponent", 27.40)
-  };
-  boolean monitor;
-  Random rg = new Random();
-  Thread runner;
+	private static final long serialVersionUID = 1L;
+	Stock[] market = { new Stock("Food", 14.57),
+			new Stock("Water", 17.44),
+			new Stock("Wood", 16.44),
+			new Stock("Iron", 7.21),
+			new Stock("Gold", 27.40) };
+	boolean monitor;
+	Random rg = new Random();
+	Thread runner;
 
-  public MyStockTest() {
-    // Not meant to be shown as a real frame
-    super("Thread only version . . .");
-    runner = new Thread(this);
-    runner.start();
-  }
+	public MyStockTest() {
+		// Not meant to be shown as a real frame
+		super("Thread only version . . .");
+		runner = new Thread(this);
+		runner.start();
+	}
 
+	public MyStockTest(boolean monitorOn) {
+		super("Stock Market Monitor");
+		setSize(400, 100);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		monitor = monitorOn;
 
-  public MyStockTest(boolean monitorOn) {
-    super("Stock Market Monitor");
-    setSize(400, 100);
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
-    monitor = monitorOn;
+		getContentPane().add(new JLabel("Trading is active.  " + "Close this window to close the market."),
+				BorderLayout.CENTER);
+		runner = new Thread(this);
+		runner.start();
+	}
 
-    getContentPane().add(new JLabel("Trading is active.  " +
-          "Close this window to close the market."),
-          BorderLayout.CENTER);
-    runner = new Thread(this);
-    runner.start();
-  }
+	public void run() {
+		while (true) {
+			int whichStock = Math.abs(rg.nextInt()) % market.length;
+			double delta = rg.nextDouble() - 0.4;
+			market[whichStock].update(delta);
+			if (monitor) {
+				market[whichStock].print();
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException ie) {
+			}
+		}
+	}
 
-  public void run() {
-    while(true) {
-      int whichStock = Math.abs(rg.nextInt()) % market.length;
-      double delta = rg.nextDouble() - 0.4;
-      market[whichStock].update(delta);
-      if (monitor) {
-        market[whichStock].print();
-      }
-      try {
-        Thread.sleep(1000);
-      }
-      catch(InterruptedException ie) {
-      }
-    }
-  }
+	public Stock getQuote(int index) {
+		return market[index];
+	}
 
-  public Stock getQuote(int index) {
-    return market[index];
-  }
+	// This method returns the list of all the symbols in the market table
+	public String[] getSymbols() {
+		String[] symbols = new String[market.length];
+		for (int i = 0; i < market.length; i++) {
+			symbols[i] = market[i].symbol;
+		}
+		return symbols;
+	}
 
-  // This method returns the list of all the symbols in the market table
-  public String[] getSymbols() {
-    String[] symbols = new String[market.length];
-    for (int i = 0; i < market.length; i++) {
-      symbols[i] = market[i].symbol;
-    }
-    return symbols;
-  }
-
-  public static void main(String args[]) {
-    MyStockTest myMarket = new MyStockTest(true);
-    myMarket.setVisible(true);
-  }
+	public static void main(String args[]) {
+		MyStockTest myMarket = new MyStockTest(true);
+		myMarket.setVisible(true);
+	}
 }
