4e5cf1f9f44eb7b606571b2c7dceb9b351d3cbe3
Krzysztof Burkat <burkat.krzysztof@gmail.com>
2017-04-11 19:29:25 +0200

    added goods and date info

--- ExchangeModule/src/exchange/Stock.java
+++ ExchangeModule/src/exchange/Stock.java
@@ -1,29 +1,41 @@
 package exchange;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/** Main exchange class */
+
 public class Stock {
 
-public Stock(String string, double d) {
-        this.symbol = string;
-        this.price = d;
-    }
+	public Stock(String string, double d) {
+		this.symbol = string;
+		this.price = d;
+	}
 
-    public String symbol;
-    public double price;
-    public double delta;
-    public String lastUpdate;
+	public String symbol;
+	public double price;
+	public double delta;
+	public String lastUpdate;
+	DateFormat date;
 
-    public void print() {
-        System.out.println(this);
-    }
+	public void print() {
+		System.out.println(this);
+	}
 
-    public void update(double delta2) {
-        this.delta = delta2;
-    }
+	public void update(double delta2) {
+		this.delta = delta2;
+		this.price = this.price + this.price  * delta2;
+		date = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		Date todaysDate = new Date();
+		lastUpdate = date.format(todaysDate);
+	}
 
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        builder.append("Stock [symbol=").append(symbol).append(", price=").append(price).append(", delta=").append(delta).append(", lastUpdate=")
-                .append(lastUpdate).append("]");
-        return builder.toString();
-    }
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("").append(symbol).append(": price = ").append(price).append(", delta = ")
+				.append(delta).append(", lastUpdate = ").append(lastUpdate);
+		return builder.toString();
+	}
 }
