af1ce4acc69fb92c7ac8b93c2dfb460645b5e195
cwiertnia.michal <cwiertnia.michal@gmail.com>
2017-04-11 22:14:54 +0200

    Improved TutorialView. Now it is not using CenterPart.

--- bgEngineView/TutorialView.py
+++ bgEngineView/TutorialView.py
@@ -2,81 +2,64 @@ import wx
 import os
 
 
-class TutorialViewCenterPart(wx.Panel):
-    def __init__(self, parent, ID, tplSize, musicPath="TwoMandolins.mp3"):
+class TutorialView(wx.Panel):
+    def __init__(self, parent, size, name, musicPath="TwoMandolins.mp3"):
+        wx.Panel.__init__(self, size=size, parent=parent)
+        self.Bind(wx.EVT_SHOW, self.onShow, self)
         self.parent = parent
-        self.ID = ID
-        self.tplSize = tplSize
+        self.name = name
+        self.size = size
         self.musicPath = musicPath
+        self.initButtons()
+        self.SetBackgroundColour((255, 255, 255))
         self.pageID = 0
-        wx.Panel.__init__(self, self.parent, self.ID, size=self.tplSize)
-        self.SetBackgroundColour((255, 255, 0))
-        self.tutorial_info = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu sapien non felis mattis\n " \
-                        "luctus. Nulla sed sapien neque. Mauris vitae urna ac tellus cursus efficitur et egestas turpis.\n" \
-                        " Etiam vel justo scelerisque, tincidunt dui ac, iaculis felis. In hac habitasse platea dictum\n" \
-                        "st. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut \n" \
-                        "ante metus, vestibulum nec commodo ac, eleifend porta tortor. Etiam diam orci, luctus ac alique\n" \
-                        "t at, porttitor nec risus. Fusce fermentum lacinia mauris, eu hendrerit ligula convallis non. \n" \
-                        "Quisque faucibus tellus ac lacus fringilla malesuada. Nulla id neque eget nisi vulputate accum\n" \
-                        "san. Quisque euismod metus pretium justo imperdiet iaculis. Cras a ante nisi. Aliquam erat vol\n" \
-                        "utpat. Nulla rutrum ut velit a placerat. Maecenas laoreet ornare lacinia. Cras ultrices mi nis\n" \
-                        "i. Etiam euismod et magna ac mattis. Cras et quam dictum, lobortis mauris eget, pretium metus\n" \
-                        ". Integer pulvinar, sem non suscipit congue, erat ipsum tincidunt mi, et aliquam lectus leo eu\n" \
-                        " mi. Fusce blandit metus at odio consequat, a suscipit urna pharetra. Sed ullamcorper orci id \n" \
-                        "lacinia bibendum.",
-                        "Suspendisse accumsan tincidunt sagittis. Etiam tempor lacus id ante interdum, vitae faucibus \n"
-                        "nulla aliquet. Donec enim risus, tincidunt eu est nec, suscipit imperdiet nisi. Quisque \n"
-                        "laoreet magna consectetur porta gravida. Nullam id felis sapien. Suspendisse orci mi, commodo\n"
-                        " in lorem quis, tincidunt vehicula metus. Morbi laoreet, lectus eu blandit pharetra, tortor\n"
-                        " ante gravida turpis, quis mollis nisi mi vitae massa. Duis tincidunt nisi a nisi luctus tempus.\n"
-                        " Vivamus euismod a tortor at tempus."
-                         ]
+        self.tutorial_info = [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu sapien non felis mattis\n " \
+            "luctus. Nulla sed sapien neque. Mauris vitae urna ac tellus cursus efficitur et egestas turpis.\n" \
+            " Etiam vel justo scelerisque, tincidunt dui ac, iaculis felis. In hac habitasse platea dictum\n" \
+            "st. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut \n" \
+            "ante metus, vestibulum nec commodo ac, eleifend porta tortor. Etiam diam orci, luctus ac alique\n" \
+            "t at, porttitor nec risus. Fusce fermentum lacinia mauris, eu hendrerit ligula convallis non. \n" \
+            "Quisque faucibus tellus ac lacus fringilla malesuada. Nulla id neque eget nisi vulputate accum\n" \
+            "san. Quisque euismod metus pretium justo imperdiet iaculis. Cras a ante nisi. Aliquam erat vol\n" \
+            "utpat. Nulla rutrum ut velit a placerat. Maecenas laoreet ornare lacinia. Cras ultrices mi nis\n" \
+            "i. Etiam euismod et magna ac mattis. Cras et quam dictum, lobortis mauris eget, pretium metus\n" \
+            ". Integer pulvinar, sem non suscipit congue, erat ipsum tincidunt mi, et aliquam lectus leo eu\n" \
+            " mi. Fusce blandit metus at odio consequat, a suscipit urna pharetra. Sed ullamcorper orci id \n" \
+            "lacinia bibendum.",
+            "Suspendisse accumsan tincidunt sagittis. Etiam tempor lacus id ante interdum, vitae faucibus \n"
+            "nulla aliquet. Donec enim risus, tincidunt eu est nec, suscipit imperdiet nisi. Quisque \n"
+            "laoreet magna consectetur porta gravida. Nullam id felis sapien. Suspendisse orci mi, commodo\n"
+            " in lorem quis, tincidunt vehicula metus. Morbi laoreet, lectus eu blandit pharetra, tortor\n"
+            " ante gravida turpis, quis mollis nisi mi vitae massa. Duis tincidunt nisi a nisi luctus tempus.\n"
+            " Vivamus euismod a tortor at tempus.",
+            "Aliquam pulvinar ac dui ut iaculis. Nullam ut rutrum odio. Etiam accumsan in dui in auctor. Praesent \n"
+            "cursus lacus nec nisl blandit ullamcorper. Proin non est efficitur ligula ultrices pellentesque. Cras \n"
+            "aliquet, ante varius commodo fermentum, nisi elit dictum lacus, ac feugiat libero felis eu mi. Nulla \n"
+            "porttitor faucibus dui quis gravida. Suspendisse potenti."
+            ]
 
         self.centerSizer = wx.BoxSizer(wx.HORIZONTAL)
         self.ctrlMsgField = wx.StaticText(self, label=self.tutorial_info[self.pageID])
         self.centerSizer.Add(self.ctrlMsgField, 0, wx.EXPAND, 5)
 
     def onShow(self, event):
+        # print "Menu on show"
+        global pygame
         if event.GetShow():
-            print "shown tutorial"
-            self.initView()
-            try:
-                pygame.mixer.init()
-                pygame.mixer.music.load(self.musicPath)
-                pygame.mixer.music.play()
-            except Exception:
-                print "Problem with music"
+            # print "menu: setting up music"
+            import pygame
+            pygame.init()
+            pygame.mixer.init()
+            pygame.mixer.music.load(self.musicPath)
+            pygame.mixer.music.play()
         else:
             try:
-                print "tutorial: quitting"
+                # print "Menu, quitting"
                 pygame.quit()
             except Exception:
-                print "first appearance of Tutorial: pygame not initialized in tutorial"
-
-    def initView(self):
-        print "Tutorial: initview"
-        global pygame
-        os.environ['SDL_WINDOWID'] = str(self.GetHandle())
-        os.environ['SDL_VIDEODRIVER'] = 'windib'
-        import pygame   # this has to happen after setting the environment variables.
-        pygame.init()
-        pygame.display.init()
-        window = pygame.display.set_mode(self.tplSize)
-        pygame.display.flip()
-        pygame.display.update()
-
-
-class TutorialView(wx.Panel):
-    def __init__(self, parent, size, name):
-        wx.Panel.__init__(self, parent=parent, size=size)
-        self.SetBackgroundColour((255, 255, 255))
-        self.name = name
-        self.parent = parent
-        self.center = TutorialViewCenterPart(self, -1, (508, 195))
-
-        self.initButtons()
-        self.Bind(wx.EVT_SHOW, self.center.onShow, self)
-        #self.initMenuBar()
+                # print "menu: problem with pygame quit"
+                pass
 
     def initButtons(self):
         """ This function creates buttons, sets theirs positions and size and
@@ -90,14 +73,24 @@ class TutorialView(wx.Panel):
 
     def nextPage(self, event):
         """ This function displays next page of tutorial """
-        if self.center.pageID + 1 < self.center.tutorial_info.__len__():
-            self.center.pageID += 1
-
+        if self.pageID + 1 < self.tutorial_info.__len__():
+            self.pageID += 1
+            self.ctrlMsgField.Destroy()
+            self.Refresh()
+            self.centerSizer = wx.BoxSizer(wx.HORIZONTAL)
+            self.ctrlMsgField = wx.StaticText(self, label=self.tutorial_info[self.pageID])
+            self.centerSizer.Add(self.ctrlMsgField, 0, wx.EXPAND, 5)
 
     def prevPage(self, event):
         """ This function displays previous page of tutorial """
-        if self.center.pageID > 0:
-            self.center.pageID -= 1
+        if self.pageID > 0:
+            self.pageID -= 1
+            self.ctrlMsgField.Destroy()
+            self.Refresh()
+            self.centerSizer = wx.BoxSizer(wx.HORIZONTAL)
+            self.ctrlMsgField = wx.StaticText(self, label=self.tutorial_info[self.pageID])
+            self.centerSizer.Add(self.ctrlMsgField, 0, wx.EXPAND, 5)
+
 
     def retToMenu(self, event):
         """ This function returns to Menu view """
