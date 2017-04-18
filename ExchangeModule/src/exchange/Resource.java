c2dba227f7abb1215edad8e9ba76363ab8a6b0a3
Krzysztof Burkat <burkat.krzysztof@gmail.com>
2017-04-18 14:11:44 +0200

    new working stock model
    no integration yet

---
+++ ExchangeModule/src/exchange/Resource.java
@@ -0,0 +1,74 @@
+package exchange;
+
+public class Resource {
+
+	public static final double defaultPrice = 100.0;
+
+	private String name;
+	private double price;
+	private double oldprice;
+	private double quantity;
+
+	Resource() {
+		super();
+	}
+
+	Resource(String name) {
+		super();
+		this.name = name;
+		this.price = defaultPrice;
+	}
+
+	Resource(String name, double price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+
+	public Resource(String name, double price, double quantity) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public double getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(double quantity) {
+		this.quantity = quantity;
+	}
+
+	public void update(double delta) {
+		this.oldprice = this.price;
+		this.price = this.price + this.price * delta;
+		printChange();
+	}
+
+	public void printChange() {
+		System.out.println(this.name+": old price = "+this.oldprice+", new price = "+this.price);
+	}
+
+	@Override
+	public String toString() {
+		return "Resource [name=" + name + ", price=" + price + ", quantityToBuy="+ quantity + "]";
+	}
+
+}
---
+++ ExchangeModule/src/exchange/Stock.java
@@ -0,0 +1,79 @@
+package exchange;
+
+import java.util.*;
+import javax.swing.JOptionPane;
+
+public class Stock {
+
+	private List<Resource> resources = new ArrayList<>();
+	private RepresenterMock player;
+	private String[] names;
+	private boolean working;
+
+	public Stock() {
+		working = true;
+	}
+
+	public List<Resource> getResources() {
+		return resources;
+	}
+
+	public boolean isWorking() {
+		return working;
+	}
+
+	public void setWorking(boolean working) {
+		this.working = working;
+	}
+
+	public void init(List<String> resourcesNames) {
+		names = new String[resourcesNames.size()];
+		int i = 0;
+		for (String name : resourcesNames) {
+			resources.add(new Resource(name));
+			names[i] = name;
+			i++;
+		}
+	}
+
+	public String[] getNames() {
+		return names;
+	}
+
+	public void setPlayer(RepresenterMock player) {
+		this.player = player;
+	}
+
+	public Resource getResource(String name) {
+		for (Resource res : resources) {
+			if (res.getName().equals(name)) {
+				return res;
+			}
+		}
+		return null;
+	}
+
+	public void stockOperation(String resourceName, double amount, String operation) {
+		if (operation.startsWith("buy")) {
+			if (player.getMoney() > amount * getResource(resourceName).getPrice()) {
+				double currentResources = player.getPlayerResources().get(resourceName);
+				player.getPlayerResources().put(resourceName, currentResources + amount);
+				player.setMoney(player.getMoney() - amount * getResource(resourceName).getPrice());
+				JOptionPane.showMessageDialog(null, "You bought " + resourceName + ", and now you have "
+						+ player.getPlayerResources().get(resourceName)+" units", "", JOptionPane.WARNING_MESSAGE);
+			} else {
+				JOptionPane.showMessageDialog(null, "Not enough money", "", JOptionPane.WARNING_MESSAGE);
+			}
+		} else {
+			if (player.getPlayerResources().get(resourceName) > amount) {
+				double currentResources = player.getPlayerResources().get(resourceName);
+				player.getPlayerResources().put(resourceName, currentResources - amount);
+				player.setMoney(player.getMoney() + amount * getResource(resourceName).getPrice());
+				JOptionPane.showMessageDialog(null, "You sold " + resourceName + ", and now you have "
+						+ player.getMoney()+" gold coins", "", JOptionPane.WARNING_MESSAGE);
+			} else {
+				JOptionPane.showMessageDialog(null, "Not enough resources", "", JOptionPane.WARNING_MESSAGE);
+			}
+		}
+	}
+}
