3a0dc292ff37a087eec762ca3c0d16155793cb6b
Robert <bielas.robert95@gmail.com>
2017-04-18 23:42:23 +0200

    exchange main

--- ExchangeModule/src/Main.java
+++ ExchangeModule/src/Main.java
@@ -1,5 +1,11 @@
+import exchangenode.ExchangeNode;
+
 //* Init class responsible for exchange (stock market) during a game. Will be implemented in future. */ 
 
 public class Main {
 
+	public static void main(String[] args){
+		ExchangeNode exchangeNode = new ExchangeNode(null, null);
+		exchangeNode.nodeLoop();
+	}
 }
