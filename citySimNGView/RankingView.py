20fc965d522361ca1f1881ecaff5d02c954c419b
rockzzz <k.mura121@gmail.com>
2017-05-16 04:18:20 +0200

    Add ranking basics, refactor .gitignore files

--- citySimNGView/RankingView.py
+++ citySimNGView/RankingView.py
@@ -1,4 +1,5 @@
 import wx
+import wx.grid as gridlib
 import os
 import json
 from RelativePaths import relative_music_path
@@ -14,9 +15,7 @@ class RankingView(wx.Panel):
         self.size = size
         self.musicPath = musicPath
         self.initRanking()
-        self.SetBackgroundColour((255, 255, 255))
-        
-        
+        self.SetBackgroundColour((255, 255, 255))        
 
     def onShow(self, event):
         # print "Menu on show"
@@ -49,12 +48,16 @@ class RankingView(wx.Panel):
         ctrlMsgField = wx.StaticText(self, label=ranking_info[0])
         centerSizer.Add(ctrlMsgField, 0, wx.CENTER)
 
-        menu_btn = wx.Button(self, label="Menu")
+        simpleGrid = gridlib.Grid(self)
+        simpleGrid.CreateGrid(20, 3)
+        simpleGrid.SetColLabelValue(0, "User")
+        simpleGrid.SetColLabelValue(1, "Money")
+        simpleGrid.SetColLabelValue(2, "Nr of games")
+        centerSizer.Add(simpleGrid, 0, wx.CENTER)
 
+        menu_btn = wx.Button(self, label="Menu")
         centerSizer.AddSpacer(50)
         centerSizer.Add(menu_btn, 0, wx.CENTER | wx.ALL, 5)
-        
-
         self.Bind(wx.EVT_BUTTON, self.retToMenu, menu_btn)
 
         self.SetSizer(centerSizer)
