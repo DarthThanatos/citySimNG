4466715f1fc549549d070787fd2707fc9fca0b02
Rockfan121 <k.mura121@gmail.com>
2017-12-17 19:21:40 +0100

    Replace default panel texture, amend tutorial pages

--- controllerModules/SkeletonModule/src/constants/CreatorConfig.java
+++ controllerModules/SkeletonModule/src/constants/CreatorConfig.java
@@ -6,6 +6,6 @@ public class CreatorConfig {
 	public static final String DEPENDENCIES_DEFAULT_SET_NAME_TWO = "Moon";
 	public static final String TEXTURE_ONE_DEFAULT_NAME = "new\\meadow1.jpg";
 	public static final String TEXTURE_TWO_DEFAULT_NAME = "new\\meadow2.jpg";
-	public static final String PANEL_TEXURE_DEFAULT_NAME = "BuildingsPanelTexture.jpg";
+	public static final String PANEL_TEXURE_DEFAULT_NAME = "new\\drewno.jpg";
 	public static final String MP3_DEFAULT_NAME = "TwoMandolins.mp3";
 }
